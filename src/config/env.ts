1d10712b228d5de3af9e1d1a338f0e36b1abe095
minhnndev <ngocminhit2000@gmail.com>
2026-01-31 08:14:58 +0700

    chore: add logging for environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,4 +2,5 @@ export const ENV = import.meta.env.VITE_ENV;
 export const API_URL = import.meta.env.VITE_API_URL;
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
+console.log('🚀 env:', import.meta.env);
 console.log(`🚀 ${ENV}:`, API_URL);
